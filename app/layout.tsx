f44335f367dcec7312a2cb2dd2be799b723de82f
Fredabod <fredrickbolutife@gmail.com>
2026-03-09 11:15:02 +0100

    Add icons to metadata for improved branding in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ export const metadata: Metadata = {
   title: "Youth Convocation Registration | The Apostolic Church",
   description:
     "Secure your participation for the upcoming Youth Convocation by The Apostolic Church.",
+  icons: {
+    icon: "/taclogo.jpeg",
+    apple: "/taclogo.jpeg",
+  },
 };
 
 export default function RootLayout({
